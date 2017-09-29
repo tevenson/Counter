781ba4df2d998fe06bcf923ec5d4409bc5981030
Thomas Evenson <tevenson@gmail.com>
2017-09-29 10:05:19 -0700

    Updated count to be 1-20.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            for(int i = 1; i < 11; i++)
+            for(int i = 1; i < 21; i++)
             {
                 Console.WriteLine(i);
             }
