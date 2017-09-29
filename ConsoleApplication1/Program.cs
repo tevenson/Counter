94e7a0ec7977bd4e882f4985f1165cf6cede6cd1
Thomas Evenson <tevenson@gmail.com>
2017-09-29 09:58:08 -0700

    Counting 1-10

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            for(int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
